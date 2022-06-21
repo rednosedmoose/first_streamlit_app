b5e62dad505e4ac75ee8d84179a58d277a56eff9
rednosedmoose <107918481+rednosedmoose@users.noreply.github.com>
2022-06-21 17:05:08 +0800

    Update streamlit_app.py
    fix selector

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 # Multilist
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index)),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display table
 streamlit.dataframe(fruits_to_show)
